1fe99cf0be6841919132c0afcc129096a80c6fc0
FinP <finpluto@hotmail.com>
2018-09-12 20:59:34 +0800

    添加i3init.sh脚本

---
+++ i3init.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#xrandr && xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --mode 1440x900 --pos 1920x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --dpi 120
+feh --bg-fill '/home/finp/Picture/ar15&sopmod2.jpg'
